1a8de55c26afa562a091957716fc85a88ca71464
leavemydogalone <niklasport88@gmail.com>
2022-06-26 22:18:37 -0700

    got jwt to work and checks set in place for updating user info

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -1,21 +1,29 @@
 const jwt = require("jsonwebtoken");
 
-//   checks for token and token validity and sends next if token is valid
 const verifyToken = (req, res, next) => {
   const authHeader = req.headers.token;
+
   if (authHeader) {
+    // Get the token after "Bearer" in the Header
+    const token = authHeader.split(" ")[1];
+
+    // verify the token
     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
       if (err) return res.status(403).json("Token is not valid");
+
+      //   save user to req.body and go next
       req.user = user;
       next();
     });
   } else {
+    // If there is no token in the Header
     return res.status(401).json("You are not authenticated!");
   }
 };
 
-// checks user's id to see if it is equal to the id in params
 const verifyTokenAndAuthorization = (req, res, next) => {
+  // Check that the user id sent from the verification is the same as the id in the parameters
+  // or that they are admin
   verifyToken(req, res, () => {
     if (req.user.id === req.params.id || req.user.idAdmin) {
       next();
